50d0830a9b5b0b4b39377e21c5e84b81f6c27d33
Simon Wörpel <simon.woerpel@medienrevolte.de>
2022-10-18 18:59:16 +0200

    Use correctiv mapbox account

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 const config = {
   mapbox: {
     token:
-      "pk.eyJ1Ijoic2ltb253b2VycGVsIiwiYSI6ImNqMHA0cGY4eDAwd3QzMm1yZm9yY3c0cWUifQ.78weiiM4vcLanNNg-v7EBA",
-    style: "mapbox://styles/simonwoerpel/cl7um4n0u007i14kbdlzl0f2x",
+      "pk.eyJ1IjoiY29ycmVjdGl2LW1hcGJveCIsImEiOiJjbDllOGhsdDIxa3oyM3dxdDkwajJ4dmh4In0.aRVZgaXG7AwgWyLDvNCYLg",
+    style: "mapbox://styles/correctiv-mapbox/cl9efy1v3005c15lapfnrulyk",
     center: [10.565834, 51.145661],
     bounds: [5.86625035, 47.2701236, 15.04181566, 55.05877772],
     zoom: 6,
     layers: ["messstellen-trends-timeseries", "districts"],
     districtLayer: "districts",
     districtLayerOutline: "districts-outline",
-    districtSource: "mapbox://simonwoerpel.1swa7nuo",
-    districtSourceLayer: "VG250_KRS_with_summaries-b3rb6g",
+    districtSource: "mapbox://correctiv-mapbox.3drmx64j",
+    districtSourceLayer: "districts-6s4e28",
     stationLayer: "messstellen-trends-timeseries",
   },
 };
